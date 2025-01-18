5e878f829faca7d7c595e75973e65124c5fd3695
leitosuarez <leoneljunio123@gmail.com>
2025-01-18 11:20:46 -0300

    terminamos la base del proyecto integrador, primera etapa

---
+++ src/db/productos.js
@@ -0,0 +1,60 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'BMW Strom 250cc',
+        foto: 'imgs/bmw strom 250.jpg',
+        descripcion: 'BMW Strom 250cc', precio: 6000
+    },
+
+    {
+        id: 2,
+        nombre: 'Dominar 400cc 2023',
+        foto: 'imgs/dominar 400.png',
+        descripcion: 'Dominar 400cc 2023', precio: 7500
+    },
+
+    {
+        id: 3,
+        nombre: 'Honda Twister CB 250cc',
+        foto: 'imgs/bmw strom 250.jpg',
+        descripcion: 'Honda Twister CB 250cc', precio: 5000
+    },
+
+    {
+        id: 4,
+        nombre: 'Kawasaki Z400',
+        foto: 'imgs/bmw strom 250.jpg',
+        descripcion: 'Kawasaki z400', precio: 7000
+    },
+
+    {
+        id: 5,
+        nombre: 'Rowser NS Bajaj 200 2024',
+        foto: 'imgs/bmw strom 250.jpg',
+        descripcion: 'Rowser ns bajaj 200 2024', precio: 4800
+    },
+
+    {
+        id: 6,
+        nombre: 'Yamaha FZ 250cc',
+        foto: 'imgs/bmw strom 250.jpg',
+        descripcion: 'BMW Strom 250cc', precio: 5200
+    },
+
+    {
+        id: 7,
+        nombre: 'Yamaha R6',
+        foto: 'imgs/yamaha r6.jpg',
+        descripcion: 'Yamaha R6', precio: 16000
+    },
+
+    {
+        id: 8,
+        nombre: 'Yamaha R1',
+        foto: 'imgs/Yamaha-YZF-R1-2020-3.jpg',
+        descripcion: 'Yamaha R1', precio: 25000
+    },
+
+]
+
+export default productos
--- src/main.js
+++ src/main.js
@@ -1,2 +1,111 @@
+import productos from './db/productos'
 import './sass/main.scss'
+
+let contenedorProductos = document.getElementById('card-container')
+
+ let start = ()=> { // primera opcion (no tan segura)
+    let html = ''
+
+    productos.forEach(prod => {
+        html += `<div class="card">
+                  <article class="card__article">
+                    <div class="card__image-container">
+                      <img class="card__image" src="${prod.foto}" alt="${prod.nombre}">
+                    </div>
+                    <div class="card__content">
+                        <h2 class="card__heading">${prod.nombre}</h2>
+                        <div class="card__description">
+                          <p>${prod.descripcion}</p>
+                        </div>
+                    </div>
+                  </article>
+                </div> <!-- .card --> \n`
+
+        
+    })
+    
+    console.log(html)
+    contenedorProductos.innerHTML = html
+} 
+
+const start2 = ()=> { //segunda opcion, mas segura ya que estoy creando todo dinamicamente
+  productos.forEach(prod => {
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+    const article = document.createElement('article')
+    article.classList.add('card__article')
+    card.appendChild(article)
+    const cardImageContainer = document.createElement('div')
+    cardImageContainer.classList.add('card_image-container')
+    article.appendChild(cardImageContainer)
+    const cardImage = document.createElement('img')
+    cardImage.classList.add('card__image')
+    cardImage.src = prod.foto
+    cardImage.alt = prod.nombre
+    cardImageContainer.appendChild(cardImage)
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card__content')
+    article.appendChild(cardContent)
+    const cardHeadind = document.createElement('h2')
+    cardHeadind.classList.add('card__headind')
+    cardHeadind.textContent = prod.nombre
+    const cardDescription = document.createElement('div')
+    cardDescription.classList.add('card__description')
+    cardContent.appendChild(cardHeadind)
+    cardContent.appendChild(cardDescription)
+    const descripcion = document.createElement('p')
+    descripcion.textContent = prod.descripcion
+    cardDescription.appendChild(descripcion)
+
+    console.log(card)
+    contenedorProductos.appendChild(card)
+  })
+  
+  
+}
+
+const start3 = ()=> { //tercera opcion (LA MAS OPTIMA) ya que se agregan los elementos no manipulando e interviniendo todo el timepo el DOM (alta carga de recursos), si no que se asigna todo una vez a una variable que esta en memoria, y esta solo cambia el dom una vez, que es cuando se agrega al contenedor html en este caso
+
+  const fragment = new DocumentFragment();
+
+  productos.forEach(prod => {
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+    const article = document.createElement('article')
+    article.classList.add('card__article')
+    card.appendChild(article)
+    const cardImageContainer = document.createElement('div')
+    cardImageContainer.classList.add('card__image-container')
+    article.appendChild(cardImageContainer)
+    const cardImage = document.createElement('img')
+    cardImage.classList.add('card__image')
+    cardImage.src = prod.foto
+    cardImage.alt = prod.nombre
+    cardImageContainer.appendChild(cardImage)
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card__content')
+    article.appendChild(cardContent)
+    const cardHeadind = document.createElement('h2')
+    cardHeadind.classList.add('card__headind')
+    cardHeadind.textContent = prod.nombre
+    const cardDescription = document.createElement('div')
+    cardDescription.classList.add('card__description')
+    cardContent.appendChild(cardHeadind)
+    cardContent.appendChild(cardDescription)
+    const descripcion = document.createElement('p')
+    descripcion.textContent = prod.descripcion
+    cardDescription.appendChild(descripcion)
+
+    fragment.appendChild(card)
+
+  })
+
+  contenedorProductos.appendChild(fragment)
+  
+  
+}
+
+window.addEventListener('DOMContentLoaded', start3)
 
