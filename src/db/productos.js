1479b75d8d55a6120e93c088e63dd32f54682829
leitosuarez <leoneljunio123@gmail.com>
2025-02-13 19:00:45 -0300

    actualizo el header

--- src/db/productos.js
+++ src/db/productos.js
@@ -3,84 +3,84 @@ const productos = [
         id: 1,
         nombre: 'BMW Strom 250cc',
         foto: 'imgs/bmw strom 250.jpg',
-        descripcion: 'Trail compacta, perfecta para aventuras urbanas y off-road ligeras.', precio: `$${7500}.00`
+        descripcion: 'Trail compacta, perfecta para aventuras urbanas y off-road ligeras.', precio: `$${7500},00`
     },
 
     {
         id: 2,
         nombre: 'Bajaj Dominar 400cc 2023',
         foto: 'imgs/dominar 400.png',
-        descripcion: 'Deportiva y touring, ideal para viajes largos con excelente potencia y confort.', precio: `$${9700}.00`
+        descripcion: 'Deportiva y touring, ideal para viajes largos con excelente potencia y confort.', precio: `$${9700},00`
     },
 
     {
         id: 3,
         nombre: 'Honda Twister CB 250cc',
         foto: 'imgs/honda twister cb 250.jpeg',
-        descripcion: 'Combinación de potencia y estilo para uso urbano.', precio: `$${5500}.00`
+        descripcion: 'Combinación de potencia y estilo para uso urbano.', precio: `$${5500},00`
     },
 
     {
         id: 4,
         nombre: 'Kawasaki Z400',
         foto: 'imgs/kawasaki z400.webp',
-        descripcion: 'Naked deportiva, ligera y con gran maniobrabilidad.', precio: `$${11000}.00`
+        descripcion: 'Naked deportiva, ligera y con gran maniobrabilidad.', precio: `$${11000},00`
     },
 
     {
         id: 5,
         nombre: 'Rowser NS Bajaj 200 2024',
         foto: 'imgs/rowser ns 200 2024.jpg',
-        descripcion: 'Equilibrio entre potencia, diseño y economía.', precio: `$${6000}.00`
+        descripcion: 'Equilibrio entre potencia, diseño y economía.', precio: `$${6000},00`
     },
 
     {
         id: 6,
         nombre: 'Yamaha FZ 250cc',
         foto: 'imgs/yamaha fz 250.webp',
-        descripcion: ' Moto versátil y moderna, perfecta para el día a día.', precio: `$${6500}.00`
+        descripcion: ' Moto versátil y moderna, perfecta para el día a día.', precio: `$${6500},00`
     },
 
     {
         id: 7,
         nombre: 'Yamaha R6',
         foto: 'imgs/yamaha r6.jpg',
-        descripcion: 'Deportiva pura con tecnología de competición.', precio: `$${20000}.00`
+        descripcion: 'Deportiva pura con tecnología de competición.', precio: `$${20000},00`
     },
 
     {
         id: 8,
         nombre: 'Yamaha R1',
         foto: 'imgs/Yamaha-YZF-R1-2020-3.jpg',
-        descripcion: 'Innovación y velocidad para los amantes de las carreras.', precio: `$${22000}.00`
+        descripcion: 'Innovación y velocidad para los amantes de las carreras.', precio: `$${22000},00`
     },
 
     {
         id: 9,
         nombre: 'Kawasaki Ninja H2R',
         foto: 'imgs/kawasaki ninja h2r.webp',
-        descripcion: 'Superdeportiva de alto rendimiento, diseñada para velocidad.', precio: `$${21000}.00`
+        descripcion: 'Superdeportiva de alto rendimiento, diseñada para velocidad.', precio: `$${21000},00`
     },
 
     {
         id: 10,
         nombre: 'CFMOTO 450SR',
         foto: 'imgs/cf-moto-600.webp',
-        descripcion: 'Magnificiencia estetica, sistema del motor respaldado por un sistema ABS conmutable.', precio: `$${19200}.00`
+        descripcion: 'Magnificiencia estetica, sistema del motor respaldado por un sistema ABS conmutable.', precio: `$${19200},00`
     },
 
     {
         id: 11,
         nombre: 'Yamaha XTZ 250cc',
         foto: 'imgs/yamaha xtz.webp',
-        descripcion: 'Ligera y resistente, ideal para terrenos difíciles.', precio: `$${8500}.00`
+        descripcion: 'Ligera y resistente, ideal para terrenos difíciles.', precio: `$${8500},00`
     },
 
     {
         id: 12,
         nombre: 'Honda XR 250 Tornado',
         foto: 'imgs/honda-xr-250-tornado.webp',
-        descripcion: 'Una moto doble propósito, ágil y resistente, perfecta para aventuras en ciudad y off-road.', precio: `$${9000}.00`
+        descripcion: 'Una moto doble propósito, ágil y resistente, perfecta para aventuras en ciudad y off-road.', precio: `$${9000},00`
     },
 
 
