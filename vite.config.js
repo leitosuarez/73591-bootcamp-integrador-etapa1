502842d7fdb4a95804e09e24bb29ccca2d7a0c06
leitosuarez <leoneljunio123@gmail.com>
2025-02-13 03:05:49 -0300

    agrego a vitecongif el nuevo archivo index de la pagina nosotros, ademas creo su js e importo su scss principal para los estilos

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default {
         rollupOptions: {
             input:{
                 main: resolve(__dirname, 'index.html'),
-                contacto: resolve(__dirname, '/pages/contacto.html'),
+                contacto: resolve(__dirname, '/pages/contacto/contacto.html'),
+                nosotros: resolve(__dirname, '/pages/nosotros/nosotros.html')
             } //es para trabajar con multipages y que para cuando lo buildiaramos y lo subamos a netlify no apareciera error de notfound (es una configuracion interna que necesita vite)
         }
     }
